eb72d4ae3538980af23d8e9c3276373d61afd4d4
udinocera <udinocera@gmail.com>
2022-03-27 21:04:52 +0200

    Salvataggio esercizio1

---
+++ jsnack1/main.js
@@ -0,0 +1,13 @@
+
+let num_1 = (prompt("Digita il primo numero."));
+let num_2 = (prompt("Digita il secondo numero."));
+
+if (num_1 > num_2){
+    alert("Questo è il numero più grande:" + " " + num_1);
+} else if (num_2 > num_1) {
+    alert("Questo è il numero più grande:" + " " + num_2);
+} else {
+    alert("Parità");
+}
+
+
